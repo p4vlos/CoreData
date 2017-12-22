5f3411e1d35174f22bfe662f9ec8aa251692f2a9
Pavlos Nicolaou <contact@pavlosnicolaou.org>
2017-12-22 19:42:41 +0200

    Initial Commit

---
+++ IntermediateTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntermediateTraining
+//
+//  Created by Pavlos Nicolaou on 22/12/2017.
+//  Copyright © 2017 Pavlos Nicolaou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
