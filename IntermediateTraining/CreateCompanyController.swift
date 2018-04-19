d8fdbefa5dd929ce1aa3eae96f80d9d3e8e8b861
Pavlos Nicolaou <pavlos@blind.org.uk>
2018-04-19 14:01:02 +0100

    editing the new view Controller

---
+++ IntermediateTraining/CreateCompanyController.swift
@@ -0,0 +1,15 @@
+//
+//  CraeteCompanyController.swift
+//  IntermediateTraining
+//
+//  Created by Pavlos Nicolaou on 19/04/2018.
+//  Copyright © 2018 Pavlos Nicolaou. All rights reserved.
+//
+
+import UIKit
+
+class CreateCompanyController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
