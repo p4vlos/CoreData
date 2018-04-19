33c95149396a96da2a415f6973b9c99b60be0408
Pavlos Nicolaou <pavlos@blind.org.uk>
2018-04-19 12:34:26 +0100

    created an empty table view Controler

--- IntermediateTraining/ViewController.swift
+++ IntermediateTraining/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         navigationItem.title = "Companies"
+        
+        tableView.backgroundColor = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
+//        tableView.separatorStyle = .none
+        
+        tableView.tableFooterView = UIView() //blank UIView
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
         
@@ -27,6 +34,19 @@ class ViewController: UIViewController {
         print("Adding")
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let tealColor = UIColor(red: 48/255, green: 164/255, blue: 164/255, alpha: 1)
+        cell.backgroundColor = tealColor
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8 //arbitrary
+    }
+    
     func setupNavigationStyle() {
         navigationController?.navigationBar.isTranslucent = false
         
@@ -35,6 +55,7 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
     }
 
 }
