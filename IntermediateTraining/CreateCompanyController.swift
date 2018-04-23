aab62072a34f44d177f157b139931a2b088fb938
Pavlos Nicolaou <pavlos@blind.org.uk>
2018-04-23 10:36:39 +0100

    choosing image from library and editing it before using it in the app

--- IntermediateTraining/CreateCompanyController.swift
+++ IntermediateTraining/CreateCompanyController.swift
@@ -16,7 +16,13 @@ protocol CreateCompanyControllerDelegate {
     func didEditCompany(company: Company)
 }
 
-class CreateCompanyController: UIViewController {
+extension UINavigationController {
+    open override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+class CreateCompanyController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var company: Company? {
         didSet {
@@ -52,6 +58,40 @@ class CreateCompanyController: UIViewController {
         dp.datePickerMode = .date
         return dp
     }()
+    
+    lazy var companyImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "select_photo_empty"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.isUserInteractionEnabled = true //remember to do this, otherwise iamge views by defualt are not interactive
+        
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectPhoto)))
+        
+        return imageView
+    }()
+    
+    @objc private func handleSelectPhoto() {
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let editedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            companyImageView.image = editedImage
+        } else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            companyImageView.image = originalImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -130,11 +170,16 @@ class CreateCompanyController: UIViewController {
         lightBlueBackroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         lightBlueBackroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         lightBlueBackroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        lightBlueBackroundView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        lightBlueBackroundView.heightAnchor.constraint(equalToConstant: 350).isActive = true
         
+        view.addSubview(companyImageView)
+        companyImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8).isActive = true
+        companyImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        companyImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        companyImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         
         view.addSubview(nameLabel)
-        nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: companyImageView.bottomAnchor).isActive = true
         nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
         nameLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
         nameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
